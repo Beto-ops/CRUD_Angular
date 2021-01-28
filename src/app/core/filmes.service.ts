96670fa782770fc78825663b616d7304fc8ee491
Humberto Rodrigues da Conceição Nagai <beto.r.conceicao@uni9.edu.br>
2021-01-27 22:40:54 -0300

    Adicionado campo de pesquisa

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
 import { ConfigPrams } from '../shared/models/config-prams';
@@ -11,6 +11,9 @@ const url = 'http://localhost:3000/filmes/';
   providedIn: 'root'
 })
 export class FilmesService {
+  visualizar(id: number) {
+    throw new Error('Method not implemented.');
+  }
   excluir(id: number) {
     throw new Error('Method not implemented.');
   }
@@ -19,5 +22,20 @@ export class FilmesService {
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
+  }
+
+  listar(pagina: number, qtdPagina: number, texto: string, genero: string): Observable<Filme[]> {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.set('_page', pagina.toString());
+    httpParams = httpParams.set('_limit', qtdPagina.toString());
+    httpParams = httpParams.set('_sort', 'id');
+    httpParams = httpParams.set('_order', 'desc');
+      if(texto) {
+    httpParams = httpParams.set('q', texto);
+      }
+      if(genero) {
+    httpParams = httpParams.set('genero', genero);
+      }
+    return this.http.get<Filme[]>(url, {params: httpParams});
   }
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { FilmesService } from 'src/app/core/filmes.service';
+import { Filme } from 'src/app/shared/models/filme';
+
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -7,13 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  constructor() { }
+  pagina = 0;
+  readonly qtdPagina = 4;
+  texto: string;
+  genero: string;
+  filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
 
-  ngOnInit() {
+
+  constructor(private filmesService: FilmesService,
+              private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+      this.texto = val;
+      this.resetarConsulta();
+    });
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.genero = val;
+      this.resetarConsulta();
+    });
+
+    this.generos = ['Ação', 'Romance', 'Terror', 'Ficção Científica', 'Comédia', 'Aventura', 'Drama'];
+
+    this.listarFilmes();
 
   }
 
-  open() {
+  onScroll(): void {
+    this.listarFilmes();
+  }
+
+  private listarFilmes(): void {
+    this.pagina++;
+    this.filmesService.listar(this.pagina, this.qtdPagina, this.texto, this.genero)
+    .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta() {
+    this.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 
 }
